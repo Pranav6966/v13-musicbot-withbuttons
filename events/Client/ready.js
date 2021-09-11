655abbe9a4d0ad4d7da8ed747b356a167616a95b
Pranav Tyagi <85784258+Pranav6966@users.noreply.github.com>
2021-09-11 22:27:33 +0530

    Update ready.js

--- events/Client/ready.js
+++ events/Client/ready.js
@@ -7,7 +7,7 @@ module.exports = async (client) => {
     client.logger.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`, "ready");
 
     //Game
-    let statuses = ['blacky', `Prefix : ${prefix}`];
+    let statuses = ['Pranav bot', `Prefix : ${prefix}`];
     setInterval(function() {
   		let status = statuses[Math.floor(Math.random()*statuses.length)];
   		client.user.setActivity(status, {type: "PLAYING"});
